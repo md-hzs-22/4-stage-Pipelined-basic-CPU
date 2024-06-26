cc1f303553c21a01e6ba5988589cb3703a1c3d51
Sabugar Mo Hamza <128530225+md-hzs-22@users.noreply.github.com>
2024-06-26 23:44:02 +0530

    Update and rename test_bench_4_pipeline.v to test_bench_4_stage_pipeline.v

---
+++ Testbench/test_bench_4_stage_pipeline.v
@@ -0,0 +1,45 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: IIT Goa
+// Engineer:
+
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module test_bench_4_stage_pipeline;
+
+	// Inputs
+	reg clk;
+	reg rst;
+	reg fileid;
+
+
+	// Outputs
+	wire [31:0] aluout;
+
+	// Instantiate the Unit Under Test (UUT)
+	pipelined_4stage uut (
+		.clk(clk), 
+		.rst(rst), 
+		.aluout(aluout)
+	);
+
+always #10 clk = ~clk;
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		rst = 0;
+		fileid = 0;
+
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+        
+
+
+	end
+      
+endmodule
